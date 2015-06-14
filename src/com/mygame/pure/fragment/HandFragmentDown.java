4d205a01acc0cb7cd144545d45432db0c5ba861d
lenovo <lenovo@idea-PC>
2015-06-15 00:00:34 +0800

    修改脸，下部视图。

--- src/com/mygame/pure/fragment/HandFragmentDown.java
+++ src/com/mygame/pure/fragment/HandFragmentDown.java
@@ -1,19 +1,21 @@
 package com.mygame.pure.fragment;
 
-import com.mygame.pure.R;
-import com.mygame.pure.R.drawable;
-import com.mygame.pure.R.id;
-import com.mygame.pure.R.layout;
-import com.mygame.pure.activity.MainActivity;
-import com.mygame.pure.utils.ToastHelper;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+
+import com.mygame.pure.R;
+import com.mygame.pure.activity.MainActivity;
+import com.mygame.pure.adapter.HistoryAdapter;
+import com.mygame.pure.utils.ToastHelper;
 
 public class HandFragmentDown extends BaseFragment implements OnClickListener{
 
@@ -29,7 +31,28 @@ public class HandFragmentDown extends BaseFragment implements OnClickListener{
 	public View onCreateView(LayoutInflater inflater,
 			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 		rootView = inflater.inflate(R.layout.tab_fragment_hand_down, container, false);
+		initView();
 		return rootView;
+	}
+	
+	private void initView() {
+		 ViewPager vpager = (ViewPager)rootView.findViewById(R.id.vPager);
+		 List<View> viewList = new ArrayList<View>();
+		 for(int i = 0; i < 3; i++){
+			 View view = View.inflate(activity, R.layout.date_record, null);
+			 viewList.add(view);
+		 }
+		 HistoryAdapter adapter = new HistoryAdapter(viewList);
+		 vpager.setAdapter(adapter);
+	}
+
+	// 点击每天显示水平
+	public void clickPageLeft(View v){
+		
+	}
+	
+	public void clickPageRight(View v){
+		
 	}
 
 	@Override
